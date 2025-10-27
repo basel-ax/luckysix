d48dcb570463e769bbef02b682d6bbccba98164a
basel-ax <mpak59rus@list.ru>
2025-10-27 17:17:46 +0700

    Upd WalletBalance

--- internal/entity/wallet_balance.go
+++ internal/entity/wallet_balance.go
@@ -4,6 +4,7 @@ import "github.com/shopspring/decimal"
 
 type WalletBalance struct {
 	ID           uint
+	Wallet       string
 	BlockchainID uint
 	LuckySixID   uint
 	Balance      decimal.Decimal `json:"amount" sql:"type:decimal(20,8);"`
