71c78b92b9a46e7b106b4fad36d739fd46a4cb0f
basel-ax <mpak59rus@list.ru>
2025-12-02 15:19:35 +0700

    update WalletBalance entity

--- entity/wallet_balance.go
+++ entity/wallet_balance.go
@@ -1,10 +1,19 @@
 package entity
 
-import "gorm.io/gorm"
+import (
+	"time"
+
+	"gorm.io/gorm"
+)
 
 // WalletBalance represents a 12-word BIP39 wallet mnemonic generated from a LuckySix combination.
 type WalletBalance struct {
 	gorm.Model
-	LuckySixID uint   `gorm:"index:idx_wallet_luckysix;uniqueIndex:idx_wallet_mnemonic"`
-	Mnemonic   string `gorm:"type:text;uniqueIndex:idx_wallet_mnemonic"`
+	LuckySixID       uint       `gorm:"index:idx_wallet_luckysix;uniqueIndex:idx_wallet_mnemonic"`
+	Mnemonic         string     `gorm:"type:text;uniqueIndex:idx_wallet_mnemonic"`
+	Address          string     `gorm:"column:address"`
+	CosmosAddress    string     `gorm:"column:cosmos_address"`
+	Balance          string     `gorm:"column:balance"` // Stored as string to handle large numbers
+	BalanceUpdatedAt *time.Time `gorm:"column:balance_updated_at"`
+	IsNotified       bool       `gorm:"column:is_notified;default:false"`
 }
